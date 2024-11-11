aa7832653af2df5d1ae5e8bb5900dd7209d19b79
ryan <ryan.xie129@gmail.com>
2024-11-10 17:59:03 -0800

    unfinished freelancer profile page

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,9 +1,41 @@
-import { Flex } from "@mantine/core";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AspectRatio, Button, Drawer, Flex, ScrollArea } from "@mantine/core";
+import { useState } from "react";
+import { useDisclosure } from '@mantine/hooks';
+import DefaultPfp from "./assets/default-pfp.jpg";
 
 export default function Profile() {
+    const [visible, setVisible] = useState(true);
+    const [opened, { open, close }] = useDisclosure(false);
+
     return (
-        <Flex>
-            
-        </Flex>
+        <>
+            <Flex bg="#42464B"
+                direction="column"
+                align="center"
+            >
+                <AspectRatio ratio={1} maw={400} mx="auto" pos="fixed" pt="15px">
+                    <img
+                        src={DefaultPfp}
+                        alt="freelancer-photo"
+                    />
+                    {visible}
+                </AspectRatio>
+                <Drawer position="bottom"
+                    opened={opened}
+                    onClose={close}
+                    title="John Doe"
+                    size="80vh"
+                    overlayProps={{ backgroundOpacity: 0, blur: 0 }}
+                    scrollAreaComponent={ScrollArea.Autosize}>
+
+                    [freelancer introduction]
+                </Drawer>
+
+
+            </Flex>
+
+            <Button onClick={open}>placeholder, implement draggable</Button>
+        </>
     )
 }
